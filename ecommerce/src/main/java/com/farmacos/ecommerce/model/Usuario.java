0045b94f75ccbb2359a36e95c05da3e09e52d623
Vinicius Reis <vini.santana.612@gmail.com>
2022-03-04 00:23:35 -0300

    criando conexão com banco e criando tabela usuario

---
+++ ecommerce/src/main/java/com/farmacos/ecommerce/model/Usuario.java
@@ -0,0 +1,52 @@
+package com.farmacos.ecommerce.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "USUARIO")
+@Entity
+public class Usuario {
+
+	private static final long serialVersionUID = -3766858690685689916L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
+	private UUID id;
+	
+	@Column(name = "NOME", nullable = false)
+	private String nome;
+	
+	@Column(name = "TELEFONE")
+	private String telefone;
+	
+	@Column(name = "EMAIL", nullable = false, unique = true)
+	private String email;
+	
+	@Column(name = "DATA_ANIVERSARIO")
+	private Date dtNascimento;
+	
+	@Column(name = "CARGO", nullable = false)
+	private String Cargo;
+	
+	@Column(name = "SENHA", nullable = false)
+	private String Senha;
+	
+}
